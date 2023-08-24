7e1f2a2d986e239b3546cdf3465a2364381509ec
jiftechnify <jiftech.stlfy@gmail.com>
2023-08-24 21:57:13 +0900

    migrate codes

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,27 +1,115 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { finishEvent, nip19, relayInit } from "nostr-tools";
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-nostr-client" is now active in the web extension host!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-nostr-client.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-nostr-client in a web extension host!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "vscode-nostr-client.setPrivKey",
+      handleSetPrivateKey(context)
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "vscode-nostr-client.postText",
+      handlePostText(context)
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+const KEY_NOSTR_PRIVATE_KEY = "nostr-priv-key";
+
+const regexp32BytesHexStr = /^[a-f0-9]{64}$/;
+
+// if `pk` is ...
+// - bech32-encoded private key ("nsec1...`), validate and convert to hex string.
+// - hex string of 32 byte data, leave it as is.
+// - otherwise, return `undefined`.
+const toHexPrivateKey = (pk: string): string | undefined => {
+  if (pk.startsWith("nsec1")) {
+    try {
+      const res = nip19.decode(pk);
+      if (res.type === "nsec") {
+        return res.data;
+      }
+      console.log("toHexPrivateKey: unexpected decode result");
+      return undefined;
+    } catch (err) {
+      console.error(err);
+      return undefined;
+    }
+  }
+  return regexp32BytesHexStr.test(pk) ? pk : undefined;
+};
+
+const handleSetPrivateKey = (context: vscode.ExtensionContext) => {
+  return async () => {
+    const k = await context.secrets.get(KEY_NOSTR_PRIVATE_KEY);
+    if (k !== undefined) {
+      const sel = await vscode.window.showQuickPick(["Yes", "No"], {
+        title: "Private key is already set. Is it OK to overwrite?",
+      });
+      if (sel === "No") {
+        return;
+      }
+    }
+
+    const input = await vscode.window.showInputBox({
+      title: "Input your Nostr private key",
+      password: true,
+      placeHolder: "hex or nsec",
+      ignoreFocusOut: true,
+    });
+    if (!input) {
+      return;
+    }
+    const hexPk = toHexPrivateKey(input);
+    if (hexPk === undefined) {
+      vscode.window.showErrorMessage("Invalid private key!");
+      return;
+    }
+    console.log(hexPk);
+    await context.secrets.store(KEY_NOSTR_PRIVATE_KEY, hexPk);
+
+    vscode.window.showInformationMessage("Saved your Nostr private key!");
+  };
+};
+
+const handlePostText = (context: vscode.ExtensionContext) => {
+  return async () => {
+    const privkey = await context.secrets.get(KEY_NOSTR_PRIVATE_KEY);
+    if (privkey === undefined) {
+      vscode.window.showErrorMessage("Set your Nostr private key first!");
+      return;
+    }
+    console.log(privkey);
+
+    const content = await vscode.window.showInputBox({
+      title: "Text to post",
+      ignoreFocusOut: true,
+    });
+    if (!content) {
+      return;
+    }
+
+    const ev = finishEvent(
+      {
+        content,
+        kind: 1,
+        tags: [],
+        created_at: Math.floor(new Date().getTime() / 1000),
+      },
+      privkey
+    );
+
+    const relay = relayInit("wss://nrelay.c-stellar.net");
+    await relay.connect();
+    await relay.publish(ev);
+
+    relay.close();
+  };
+};
